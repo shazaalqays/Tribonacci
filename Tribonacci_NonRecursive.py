2012588ae6972aeee9e62c080397f75443017b88
shazaalqays <37289506+shazaalqays@users.noreply.github.com>
2022-11-27 23:04:40 +0300

    Update Tribonacci_NonRecursive.py

--- Tribonacci_NonRecursive.py
+++ Tribonacci_NonRecursive.py
@@ -3,29 +3,29 @@
 # Function Return Tribonacci Sequence
 def NRfunc(num):
     # sumlist for storing sequence
-    sumlist = [];
+    sumlist = []
     for i in range(num):
         if i == 0 or i == 1 or i == 2:
             # sum for summing the sequence
-            sum = 1;
-            sumlist.append(sum);
+            sum = 1
+            sumlist.append(sum)
         else:
-            sum = sumlist[i-1] + sumlist[i-2] + sumlist[i-3];
-            sumlist.append(sum);
+            sum = sumlist[i-1] + sumlist[i-2] + sumlist[i-3]
+            sumlist.append(sum)
     return sumlist
 
-num = 5;
-NonRecursive = [];
+num = 5
+NonRecursive = []
 if num >= 0:
-    NonRecursive = NRfunc(num);
+    NonRecursive = NRfunc(num)
     for i in NonRecursive:
         print(i)
     if num > 2:
-        print('Non-Recursive Tribonacci ', NonRecursive[-1] + NonRecursive[-2] + NonRecursive[-3]);
+        print('Non-Recursive Tribonacci ', NonRecursive[-1] + NonRecursive[-2] + NonRecursive[-3])
     else:
-        print('Non-Recursive Tribonacci ', 1);
+        print('Non-Recursive Tribonacci ', 1)
 else:
-    print('You entered negative number');
+    print('You entered negative number')
 
 
 
